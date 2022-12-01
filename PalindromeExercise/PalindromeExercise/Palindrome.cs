9a42b7f4470f6ceaacf85fa93c332f616c8c5bca
Grant Bradley <grantbradleytiger13@gmail.com>
2022-12-01 09:11:19 -0500

    Initial Commit

---
+++ PalindromeExercise/PalindromeExercise/Palindrome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class Palindrome
+    {
+        public bool IsAPalindrome(string word)
+        {
+            var lowerWord = word.ToLower();
+            var reversed = "";
+
+            for ( int i = word.Length - 1; i>= 0; i--) 
+            {
+                reversed += word[i];
+            }
+
+            if(reversed.ToLower() == lowerWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;   
+            }
+        }
+
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +7,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("blank", false)]
+        [InlineData("Hannah", true)]
+        [InlineData("Robert", false)]
+        public void MyTest(string word, bool expected)
         {
+            var test = new Palindrome();
 
+            bool actual = test.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual); 
         }
     }
 }
